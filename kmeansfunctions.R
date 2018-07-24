267a5eea2b373ddf8060f423a35f85c6ca8a79fd
Patrick Anthony Reil <35788481+patattack86@users.noreply.github.com>
2018-07-24 09:37:12 -0400

    Update kmeansfunctions.R

--- kmeansfunctions.R
+++ kmeansfunctions.R
@@ -2,7 +2,10 @@ getwd()
 
 rast_image = "M:\\GS\\Projects\\09001_ISM_P3\\RemoteSensing\\WORKSPACE\\Xenia\\BS567608.tif"
 
-ndvifun <- function(file){
+set.seed(25)
+
+ndvikmean <- function(file){
+  ###create the ndvi 
   imported_raster = brick(file)
   minmax_image = setMinMax(imported_raster)
   
@@ -11,13 +14,19 @@ ndvifun <- function(file){
   
   NDVI = (band5 - band4)/(band5 + band4)
   NDVI = na.omit(NDVI)
-}
+  
+  ###perform the kmeans
 
-kmeansfun <- function(file){
-  nr <- getValues(file)
-  set.seed(25)
-  kmncluster =  kmeans(na.omit(nr), centers = 5, iter.max = 100, nstart = 5, algorithm="Lloyd")
+  nr <- getValues(NDVI)
+  str(nr)
+  kmncluster =  kmeans(na.omit(nr), centers = 7, iter.max = 100, nstart = 5, algorithm="Lloyd")
   
   knr <- NDVI
   knr[] <- kmncluster$cluster
+  
+  #export the results
+  writeRaster(knr, "knrtest", format = "GTiff")
 }
+
+
+ndvikmean(rast_image)
