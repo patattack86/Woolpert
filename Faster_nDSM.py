bd6d1ca104e4bfd42826c7277a0882e7fd91ad76
Patrick Anthony Reil <35788481+patattack86@users.noreply.github.com>
2018-10-10 10:10:19 -0400

    Update Faster_nDSM.py

--- Faster_nDSM.py
+++ Faster_nDSM.py
@@ -1,7 +1,3 @@
-#Working to make our nDSM be created faster from original las files, theory is to break file containing the las files
-#down into a list of lists, then save each smaller list to a batch file, then execute original script on 
-#the smaller list...or sum chit idk
-
 # Import arcpy module
 import arcpy, os, sys, shutil, string, time, glob, numpy
 import numpy
@@ -16,13 +12,10 @@ print 'START ' + time.ctime()
 start_time = time.time()
 
 las_folder = r"P:\GS\Restore\Shuler_76001_MartinCo_FL\1_block_ground"
-gdb_folder = r"B:\Florida-Hydro\nDSM-Junk"
-tiff_folder = r"B:\Florida-Hydro\nDSM-Destination"
-ground_feature_dataset = "Ground"
-nonground_feature_dataset = "NonGround"
 projection = "2236"
 cellsize = "1"
-path = ""
+output_location = ""
+
 
 
 laslist = glob.glob(las_folder + "\\*.las")
@@ -32,4 +25,5 @@ l = numpy.array_split(numpy.array(laslist), 6)
 for index, List in enumerate(l):
     with open(r"C:\Users\reil\Desktop\nDSM_{}.bat".format(index), 'w') as batch_file:
         for path in list(List):
-            batch_file.write("python c:\users\reil\source\repos\PythonApplication19\PythonApplication19\create_ndsm.py {} {} {} \n".format(path, projection, cellsize))
+            las_file = path
+            batch_file.write("python c:\users\reil\source\repos\PythonApplication19\PythonApplication19\create_ndsm.py {} {} {} {} \n".format(las_file, output_location, projection, cellsize))
