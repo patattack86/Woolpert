de3822aeb426342197e740d75890793c3f038ecf
Patrick Anthony Reil <35788481+patattack86@users.noreply.github.com>
2018-10-17 16:12:11 -0400

    Update Merge_vector_raster.py

--- Merge_vector_raster.py
+++ Merge_vector_raster.py
@@ -11,6 +11,10 @@ eCog_Folder = r"B:\Florida-Hydro\Florida-Ecog"
 #Empty lists which the vector data will stored in until merged
 Raw_Vect = []
 Smooth_Vect = []
+River = []
+Stream = []
+SubStream = []
+Marshes = []
 
 #Path where the merged vectors will be stored
 Raw_merge = r"B:\Florida-Hydro\Final_Merged_Vectors\Raw_Merged.shp"
@@ -35,11 +39,20 @@ for subdir, dirs, files in os.walk(Vec_Folder):
 
         if shortname == "hydro":
             Raw_Vect.append(os.path.join(subdir,file))
+        if shortname == "Marsh":
+            River.append(os.path.join(subdir,file))
+        if shortname == "River":
+            Marshes.append(os.path.join(subdir,file))
+        if shortname == "Stream":
+            Stream.append(os.path.join(subdir,file))
+        if shortname == "Sub-Streams":
+            SubStream.append(os.path.join(subdir,file))
         elif shortname == "Hydro_smoothed":
             Smooth_Vect.append(os.path.join(subdir,file))
 
-#arcpy.Merge_management(Raw_Vect, Raw_merge)
-#arcpy.Merge_management(Smooth_Vect, Smooth_merge)
+arcpy.Merge_management(Raw_Vect, Raw_merge)
+arcpy.Merge_management(Smooth_Vect, Smooth_merge)
+
 
 #This second section is constructed to merge the raster files (Intensity & nDSM) if they exist in the
 # vector files. Honestly, this should be turned into classes with inheritence but fuckit
@@ -67,37 +80,5 @@ for subdir, dirs, files in os.walk(eCog_Folder):
                 Intensity.append(intensity_file)
 
 
-arcpy.MosaicToNewRaster_management(Intensity, Intensity_path, Intensity_file, projection,"8_BIT_UNSIGNED", "1", "1", "First")
-arcpy.MosaicToNewRaster_management(nDSM, nDSM_path, nDSM_file, projection, "8_BIT_UNSIGNED", "1", "1", "First")
-
-#arcpy.Merge_management(Raw_Vect, Raw_merge)
-#arcpy.Merge_management(Smooth_Vect, Smooth_merge)
-
-#This second section is constructed to merge the raster files (Intensity & nDSM) if they exist in the
-# vector files. Honestly, this should be turned into classes with inheritence but fuckit
-folder_names = []
-for subdir, dirs, files in os.walk(Vec_Folder):
-    for file in files:
-        (filepath, filename) = os.path.split(file)
-        (shortname, extension) = os.path.splitext(filename)
-        (path, foldername) = os.path.split(subdir)
-        folder_names.append(foldername)
-
-#Empy list containing individual nDSM and Intensity files 
-nDSM = []
-Intensity = []
-Projection = ""
-
-for subdir, dirs, files in os.walk(eCog_Folder):
-    for dir in dirs:
-        if dir in folder_names:
-            ndsm_file = "{}\\{}\\nDSM.tif".format(eCog_Folder, dir)
-            intensity_file = "{}\\{}\\Intensity.tif".format(eCog_Folder, dir)
-            if ndsm_file not in nDSM:
-                nDSM.append(ndsm_file)
-            if intensity_file not in Intensity:
-                Intensity.append(intensity_file)
-
-
-arcpy.MosaicToNewRaster_management(Intensity, Intensity_path, Intensity_file, "1")
-arcpy.MosaicToNewRaster_management(nDSM, nDSM_path, nDSM_file, "1")
+arcpy.MosaicToNewRaster_management(Intensity, Intensity_path, Intensity_file, projection,"32_BIT_UNSIGNED", "1", "1", "First")
+arcpy.MosaicToNewRaster_management(nDSM, nDSM_path, nDSM_file, projection, "32_BIT_UNSIGNED", "1", "1", "First")
