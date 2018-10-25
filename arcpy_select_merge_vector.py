efa7a446e7ea4d63ccff1e3c041523c854771dbb
Patrick Anthony Reil <35788481+patattack86@users.noreply.github.com>
2018-10-25 12:26:54 -0400

    Update arcpy_select_merge_vector.py

--- arcpy_select_merge_vector.py
+++ arcpy_select_merge_vector.py
@@ -6,20 +6,30 @@ import arcpy.cartography as CA
 import arcpy.management as DM
 
 #set parameters poopy fart butt
-Shapefile = ""
+Shapefile_folder = ""
+Vector_location = ""
 Where_Clause = ""
-Field_description = ""
+Field_name = ""
 Save_location = ""
 
 #set workspace
 arcpy.env.workspace = ""
 
+#merge individual vectors into one vector
+vector_list = []
+
+for subdir, dirs, files in os.walk(Shapefile_folder):
+    for file in files:
+        vector_list.append(os.path.join(subdir, file))
+
+merged_raw = arcpy.Merge_management(vector_list, Save_location)
+
 #create necessary field in table
-arcpy.management.AddField(Shapefile, Field_description)
+arcpy.management.AddField(merged_raw, Field_name, "SHORT")
 
 #Calculate filed bas on area
 
-arcpy.MakeFeatureLayer_management(Shapefile, "lyr")
+arcpy.MakeFeatureLayer_management(merged_raw, "lyr")
 
 Vector_selection = arcpy.SelectLayerByAttribute_management("lyr", "SUBSET_SELECTION", Where_Clause) 
 
@@ -30,3 +40,4 @@ arcpy.CopyFeatures_management(Vector_selection, "Suitable_hydro")
 #merge data based on attribute
 
 arcpy.merge("Suitable_hydro", Save_location)
+
