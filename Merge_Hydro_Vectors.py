5147499f0f18c97e410ac72784c6a8f660d5dda9
Patrick Anthony Reil <35788481+patattack86@users.noreply.github.com>
2019-09-26 15:19:33 -0400

    Update Merge_Hydro_Vectors.py

--- Merge_Hydro_Vectors.py
+++ Merge_Hydro_Vectors.py
@@ -1,3 +1,7 @@
+#Not sure which one I should keeo
+
+#Versionb one
+
 import os, shutil, time, glob, sys
 from shutil import copyfile
 import arcpy
@@ -29,3 +33,25 @@ for subdir, dirs, files in os.walk(Vec_Folder):
 arcpy.Merge_management(Raw_Vect, Raw_merge)
 arcpy.Merge_management(Smooth_Vect, Smooth_merge)
 
+#version 2
+
+import arcpy
+import numpy
+import os, shutil, time, glob, sys
+from shutil import copyfile
+
+polygon_folder = r""
+
+shapefile_list = glob.glob(polygon_folder + "\\*.shape")
+merge_output_file = r""
+
+for shapefile in shapefile_list:
+    (filepath, filename) = os.path.split(lasfile)
+    (shortname, extension) = os.path.splitext(filename)
+
+    output_gdb = shortname + ".gdb"
+    output_gdb_location = gdb_folder + "\\" + output_gdb
+
+    arcpy.CreateFileGDB_management(gdb_folder, output_gdb, "10.0")
+
+    arcpy.Merge_management(shapefile_list, merge_output_file)
