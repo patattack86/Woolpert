54764f498ddae9a4a43fcbc33e3213fd080ce2be
Patrick Anthony Reil <35788481+patattack86@users.noreply.github.com>
2019-09-30 10:44:06 -0400

    Update Merge_Hydro_Vectors.py

--- Merge_Hydro_Vectors.py
+++ Merge_Hydro_Vectors.py
@@ -8,19 +8,28 @@ polygon_folder = r""
 feature_classes = []
 
 #change this from Line to Polygon as necessary
-shape_type = ""
+shape_type = "Polygon"
 
-shapefile_list = glob.glob(polygon_folder + "\\*.shp")
+#path to the output merged file
 merge_output_file = r""
 
+#Path to dissolved file
+Diss_file_output = r""
+
+#PAth to smoothed features
 
 
 #Walking workspace recursively checking type, and appending filepath to list  
 for dirpath, dirnames, filenames in arcpy.da.Walk(polygon_folder, datatype="FeatureClass", type= shape_type):  
     for filename in filenames:  
-        desc = arcpy.Describe(os.path.join(dirpath, filename)  
+        desc = arcpy.Describe(os.path.join(dirpath, filename))
         if desc.shapeType == shape_type:
             feature_classes.append(os.path.join(dirpath, filename))  
             
 #merging my list of feature classes to a new dataset  
-arcpy.Merge_management(feature_classes, "C:\Path\MergedPolygons") 
+arcpy.Merge_management(feature_classes, merge_output_file) 
+
+#dissolving merged boundaries
+arcpy.Dissolve_management(merge_output_file, Diss_file_output)
+
+arcpy.SmoothPolygon_cartography(Diss_file_output, )
